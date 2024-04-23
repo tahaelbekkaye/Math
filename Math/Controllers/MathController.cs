5950512083c74082d15dd6fe15a211ba5478ec16
elbekkaye taha <elbekkayetaha26@gmail.com>
2024-04-23 20:31:50 +0100

    Ajoutez des fichiers projet.

---
+++ Math/Controllers/MathController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Math.Controllers
+{
+    public class MathController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Math/ShowNumber/{id}")]
+        public IActionResult ShowNumber(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+        [Route("Math/ShowSum/{num1}/{num2}")]
+        public IActionResult ShowSum(int num1, int num2)
+        {
+            int result;
+            result = num1+num2;
+            ViewBag.Num = result;
+            return View();
+        }
+        public IActionResult Equation()
+        {
+            return View();
+        }
+    }
+}
